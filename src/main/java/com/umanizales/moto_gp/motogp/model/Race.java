37974b31a350644dedec44fcba7116c717bcb057
JeronimoMN <jmurillo93220@umanizales.edu.co>
2022-11-17 12:53:43 -0500

    Third commit

--- src/main/java/com/umanizales/moto_gp/motogp/model/Race.java
+++ src/main/java/com/umanizales/moto_gp/motogp/model/Race.java
@@ -12,8 +12,47 @@ public class Race {
     private String state;
     private ListDE listBikes;
 
-    public void ForwardBikes()
+    public void ForwardBikes(SelectBike selectBike, int position)
     {
+        if(head != null)
+        {
+            NodeDE temp= head;
+            while (temp != selectBike)
+            {
+                temp= temp.getNext();
+            }
+            if(position == listBikes)
+            {
+                temp.setNext(temp.getNext());
+                temp.setNext(temp.getPrevious());
+            }
+        }
+    }
 
+    public void LostPosition()
+    {
+        NodeDE temp= head;
+        while (temp != selectBike)
+        {
+            temp= temp.getNext();
+        }
+        if(position == listBikes)
+        {
+            temp.setNext(temp.getNext());
+            temp.setNext(temp.getPrevious());
+        }
+    }
+    }
+
+    public ListDE getListBikes() {
+        return listBikes;
+    }
+
+    public Clasification getClasification() {
+        return clasification;
+    }
+
+    public void getClasi()
+    {
     }
 }
